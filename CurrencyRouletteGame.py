fb316714ce22f4501992f843112bf338f9fdda6b
ssingh9631 <sachin.singh@cognyte.com>
2024-08-22 11:34:16 +0530

    Update CurrencyRouletteGame.py

--- CurrencyRouletteGame.py
+++ CurrencyRouletteGame.py
@@ -1,11 +1,3 @@
-# 1. get_money_interval -Will get the current currency rate from USD to ILS and will
-# generate an interval as follows:
-# a. for given difficulty d, and total value of money t the interval will be: (t - (5 - d), t +
-# (5 - d))
-# 2. get_guess_from_user - A method to prompt a guess from the user to enter a guess of
-# value to a given amount of USD
-# 3. play - Will call the functions above and play the game. Will return True / False if the user
-# lost or won.
 import random
 import requests, json
 from GuessGame import generate_number
